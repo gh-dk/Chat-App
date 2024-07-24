889cce9470f91365faed7fae83fb2aede95b6668
Priyanshu Tiwari <priyanshu.tiwari@neosoftmail.com>
2024-07-24 12:59:45 +0530

    added ionic

---
+++ neo-chat/capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'Neo chat',
+  webDir: 'dist'
+};
+
+export default config;
